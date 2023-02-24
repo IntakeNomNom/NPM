64f821df171f96edc2b7de7ce5071992ef2ce206
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-24 04:05:08 +0200

    Prettifies everything

--- intake/packages/nomnom/dist/index.js
+++
@@ -1,1 +0,0 @@
-import a from"@intakenomnom/read";const o=async t=>await Promise.all(Array.from(t).filter(r=>r.endsWith(".csv")).map(r=>a(r)));var i=o;export{i as default};
--- intake/packages/read/dist/index.js
+++
@@ -1,1 +0,0 @@
-import*as r from"fs";import s from"@intakenomnom/parse";var i=async a=>{const t=await r.promises.readFile(a,"utf8");return await s(t)};export{i as default};
