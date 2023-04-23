cf9065c2da87dfc8f96e3af8df61731b1e98cc7e
Nikola Hristov <nikola@nikolahristov.tech>
2023-04-23 11:15:50 +0300

    squash!

--- intake/packages/read/dist/index.js
+++ intake/packages/read/dist/index.js
@@ -1,1 +1,1 @@
-import*as r from"fs";import s from"@intakenomnom/parse";var i=async a=>{const t=await r.promises.readFile(a,"utf8");return await s(t)};export{i as default};
+import r from"@intakenomnom/parse";import{readFile as o}from"fs/promises";var c=async a=>{const t=await o(a,"utf8");return await r(t)};export{c as default};
