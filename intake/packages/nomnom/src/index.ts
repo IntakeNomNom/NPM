80532214d10f9f0ddf5f7e112340f59a8387d1ae
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-20 14:59:43 +0300

    squash!

--- intake/packages/nomnom/src/index.ts
+++ intake/packages/nomnom/src/index.ts
@@ -4,5 +4,5 @@ export default async (files: string[] | Set<string>) =>
 	await Promise.all(
 		Array.from(files)
 			.filter((file) => file.endsWith(".csv"))
-			.map((file) => read(file)),
+			.map((file) => read(file))
 	);
