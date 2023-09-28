41fe9ffcf49a8e9e50cb69a30faa5007d96632e0
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-28 23:27:12 +0300

    squash!

--- nomnom/examples/csv/Target/Index.js
+++ nomnom/examples/csv/Target/Index.js
@@ -1,1 +1,4 @@
-import m from"@intakenomnom/nomnom";m(["./data/samples.csv"]);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nomnom_1 = require("@intakenomnom/nomnom");
+(0, nomnom_1.default)(["./data/samples.csv"]);
